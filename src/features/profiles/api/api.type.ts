09599f1302ad9da046ba5a56e55d1750d4af1b0d
ykh4933 <ykh4933@smartscore.kr>
2025-02-11 16:50:13 +0900

    feat: 프로필 API 요청 및 응답 타입 정의 추가

--- src/features/profiles/api/api.type.ts
+++ src/features/profiles/api/api.type.ts
@@ -1,5 +1,23 @@
 import type { ApiResponse } from "@/shared/api/response.type";
+import type { Profile } from "@/entities/profiles/model/profile.type";
 
+// 공유 많은 순, 최신 순
+export type SortType = "SHARE_DESC" | "LATEST";
+
+export type GetProfileRequest = {
+  page?: number;
+  perPage?: number;
+  sort?: SortType;
+};
+export type GetProfiles = ApiResponse<{
+  profiles: Array<Profile>;
+  page: number;
+  perPage: number;
+  first: boolean;
+  last: boolean;
+}>;
+
+export type CreateProfileRequest = Profile;
 export type CreateProfileResponse = ApiResponse<null>;
 
 export type DeleteProfileResponse = ApiResponse<null>;
