6e4d44be9cc394bd3f3e9ea96778034873dabb84
happhee <ghdtj7@naver.com>
2025-02-13 22:06:00 +0900

    feat: query provider app에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import AuthButton from "@/features/auth/ui/AuthButton";
 import { ExternalLink } from "@/shared/ui/ExternalLink";
 import "@/application/globals.css";
 import { Logo } from "@/shared/ui/Logo";
+import { QueryProvider } from "@/shared/lib/providers/QueryProvider";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -22,34 +23,36 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="flex min-h-screen flex-col items-center">
-          <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-            <div className="flex w-full max-w-5xl items-center justify-between px-4 text-sm">
-              <Logo />
-              <AuthButton />
-            </div>
-          </nav>
-
-          {children}
-
-          <footer className="flex w-full justify-center border-t border-t-foreground/10 p-4 text-center text-xs">
-            <div className="text-left text-stone-400">
-              <div>상호명: 마중물 | 대표: 황경찬</div>
-              <div>사업자등록번호: 264-01-01901</div>
-              <div>정보통신업 주소: 경기도 광주시 회안대로 350-23</div>
-              <div>문의: 010 5056 2412</div>
-              <div className="flex justify-between">
-                <ExternalLink href="https://slashpage.com/pec/terms-of-use">
-                  이용약관
-                </ExternalLink>
-                <ExternalLink href="https://slashpage.com/pec/privacy-policy">
-                  개인정보 처리방침
-                </ExternalLink>
+        <QueryProvider>
+          <main className="flex min-h-screen flex-col items-center">
+            <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+              <div className="flex w-full max-w-5xl items-center justify-between px-4 text-sm">
+                <Logo />
+                <AuthButton />
               </div>
-              Copyright© PEC
-            </div>
-          </footer>
-        </main>
+            </nav>
+
+            {children}
+
+            <footer className="flex w-full justify-center border-t border-t-foreground/10 p-4 text-center text-xs">
+              <div className="text-left text-stone-400">
+                <div>상호명: 마중물 | 대표: 황경찬</div>
+                <div>사업자등록번호: 264-01-01901</div>
+                <div>정보통신업 주소: 경기도 광주시 회안대로 350-23</div>
+                <div>문의: 010 5056 2412</div>
+                <div className="flex justify-between">
+                  <ExternalLink href="https://slashpage.com/pec/terms-of-use">
+                    이용약관
+                  </ExternalLink>
+                  <ExternalLink href="https://slashpage.com/pec/privacy-policy">
+                    개인정보 처리방침
+                  </ExternalLink>
+                </div>
+                Copyright© PEC
+              </div>
+            </footer>
+          </main>
+        </QueryProvider>
       </body>
     </html>
   );
