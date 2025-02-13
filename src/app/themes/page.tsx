09a116f0fd09dc4ccd5811351e24b40e96c596e8
happhee <ghdtj7@naver.com>
2025-02-13 22:07:36 +0900

    feat : 테마 목록 페이지 구현

--- src/app/themes/page.tsx
+++ src/app/themes/page.tsx
@@ -1,10 +1,22 @@
 /**
  * 테마 목록 페이지
- * 
+ *
  * - SettingDropdown 사용
  * - SelectThemeList 사용
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
+
+"use client";
+
+import { ThemeThumbnailList } from "@/entities/themes/ui/ThemeThumbnailList";
+
+export default function ThemesPage() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <ThemeThumbnailList />
+    </main>
+  );
+}
