36272ee079af3bc1d7cb36cf05ea5c054f5dc7d4
happhee <ghdtj7@naver.com>
2025-02-14 18:13:58 +0900

    fix : square 관련 변환로직 수정

--- src/entities/themes/model/transformPatternToClassName.ts
+++ src/entities/themes/model/transformPatternToClassName.ts
@@ -1,13 +1,15 @@
-import type { PatternTypeServer } from '../api/types';
+import type { ThemePattern } from "../api/types";
 
-export const transformPatternToClassName = (pattern: PatternTypeServer): string => {
+export const transformPatternToClassName = (pattern: ThemePattern): string => {
   const baseClass = `bg-pattern-${pattern}`;
-  
+
   // solid와 gradient는 size 클래스가 필요없음
-  if (pattern === 'solid' || pattern === 'gradient') {
+  if (pattern === "solid" || pattern === "gradient") {
     return baseClass;
   }
-  
+  if (pattern === "square") {
+    return `${baseClass} bg-size-square bg-pos-square`;
+  }
   // 나머지 패턴들은 size 클래스도 필요
   return `${baseClass} bg-size-${pattern}`;
-}; 
+};
