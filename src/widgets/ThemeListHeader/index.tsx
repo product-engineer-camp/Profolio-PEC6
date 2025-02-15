4569591d71fe1ac8b031159a7fab286e7f1ddf52
happhee <ghdtj7@naver.com>
2025-02-15 16:13:08 +0900

    refactor : ts-pattern으로 선언적으로 분리

--- src/widgets/ThemeListHeader/index.tsx
+++ src/widgets/ThemeListHeader/index.tsx
@@ -1,5 +1,6 @@
 import { DefaultThemeListActionDropdown } from "@/features/themes/ui/DefaultThemeListActionDropdown";
 import { DeleteThemeListActionHeader } from "@/features/themes/ui/DeleteThemeListActionHeader";
+import { match } from "ts-pattern";
 
 type ThemeListHeaderProps = {
   variant: "default" | "delete";
@@ -8,13 +9,15 @@ type ThemeListHeaderProps = {
 export const ThemeListHeader = ({ variant }: ThemeListHeaderProps) => {
   return (
     <header className="flex w-full items-center justify-between border-b p-4">
-      {variant === "default" && (
-        <>
-          <h1 className="text-xl font-bold text-black">테마</h1>
-          <DefaultThemeListActionDropdown />
-        </>
-      )}
-      {variant === "delete" && <DeleteThemeListActionHeader />}
+      {match(variant)
+        .with("default", () => (
+          <>
+            <h1 className="text-xl font-bold text-black">테마</h1>
+            <DefaultThemeListActionDropdown />
+          </>
+        ))
+        .with("delete", () => <DeleteThemeListActionHeader />)
+        .exhaustive()}
     </header>
   );
 };
