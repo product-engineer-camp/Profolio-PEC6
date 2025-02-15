a41577c772f984089317c62d02dab0eb087a34b7
happhee <ghdtj7@naver.com>
2025-02-15 11:11:11 +0900

    feat : 테마 삭제 페이지 구현

--- src/app/themes/delete/page.tsx
+++ src/app/themes/delete/page.tsx
@@ -1,12 +1,25 @@
 /**
  * 테마 삭제 페이지
- * 
- * SelectThemeCardProvider 사용 후 
+ *
+ * SelectThemeCardProvider 사용 후
  * 내부에서
  * - DeleteThemeButton 사용
  * - SelectThemeList 사용
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
+
+"use client";
+
+import { ThemeListHeader } from "@/widgets/ThemeListHeader";
+
+export default function ThemeDeletePage() {
+  return (
+    <>
+      <ThemeListHeader variant="delete" />
+      {/* ... 나머지 컴포넌트들 */}
+    </>
+  );
+}
