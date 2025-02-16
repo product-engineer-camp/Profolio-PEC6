f057f2f0bcbc37e6395a4a4d61146ec62046418d
gahui yeom <ykh4933@naver.com>
2025-02-16 20:04:40 +0900

    refactor: dialog, sonner 텍스트 색상 및 배경 색상 수정

--- src/shared/ui/sonner.tsx
+++ src/shared/ui/sonner.tsx
@@ -10,14 +10,14 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme="light"
       className="toaster group"
       position="top-center"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+            "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-500 group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-gray-500",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
